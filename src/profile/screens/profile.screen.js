5604d040ccc1f5b483c0f778e971d3758592534d
XeeshanAnsari <zeeshanansari38244@gmail.com>
2019-07-08 18:42:08 +0500

    added logout button in the profile page

--- src/profile/screens/profile.screen.js
+++ src/profile/screens/profile.screen.js
@@ -55,18 +55,26 @@ class Profile extends React.Component {
   /**
    * when user move to password change screen
    */
-
   _moveToPasswordChange = () => {
     const { navigation } = this.props;
     navigation.navigate('PasswordChange');
   };
 
+  /**
+   * when user logout
+   */
+  _handleLogout = () => {};
+
   render() {
     const { firstName, lastName, email } = this.state;
     return (
       <Container>
-        <Content padding={20} center>
-          <Div width="100%">
+        <Content
+          padding={20}
+          alignItems="center"
+          justifyContent="space-between"
+        >
+          <Div flex={1} center width="100%">
             <CustomInputText
               value={firstName}
               placeholder="First name"
@@ -113,6 +121,15 @@ class Profile extends React.Component {
                 onPress={this._moveToPasswordChange}
               />
             </Div>
+          </Div>
+          <Div width="100%">
+            <Button
+              title="Logout"
+              width="100%"
+              marginVertical={15}
+              backgroundColor="#CD3228"
+              onPress={this._handleLogout}
+            />
           </Div>
         </Content>
       </Container>
