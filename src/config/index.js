a26ebe4fb48deb6d252a305741236e80214509b1
XeeshanAnsari <zeeshanansari38244@gmail.com>
2019-07-13 19:02:26 +0500

    change base url with heroku server

--- src/config/index.js
+++ src/config/index.js
@@ -1,1 +1,2 @@
-export const BASE_URL = 'http://192.168.8.100:3000/api/v1';
+// export const BASE_URL = 'http://192.168.8.100:3000/api/v1';
+export const BASE_URL = 'https://paprclip.herokuapp.com/api/v1';
