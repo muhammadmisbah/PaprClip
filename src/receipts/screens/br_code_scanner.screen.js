db8e5885c04414fba71ac745d10218918000b245
Muhammad Misbah <muhammadmisbah107@gmail.com>
2019-09-16 21:11:34 +0500

    modified pin code input field

--- src/receipts/screens/br_code_scanner.screen.js
+++ src/receipts/screens/br_code_scanner.screen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Image, StyleSheet, View, Text, TouchableOpacity, TextInput, ScrollView, Dimensions, ActivityIndicator } from 'react-native';
+import { Image, StyleSheet, View, Text, TouchableOpacity, TextInput, ScrollView, Dimensions, ActivityIndicator, Keyboard } from 'react-native';
 import QRCodeScanner from 'react-native-qrcode-scanner';
 import { Div, CustomText, FlatButton } from 'common';
 import QRImage from './img/qr_code.png';
@@ -21,14 +21,98 @@ class BarCodeScannerComponent extends React.Component {
     this.state = {
       pin: "",
       error: "",
-      loader: false
+      loader: false,
+      code1: '',
+      code2: '',
+      code3: '',
+      code4: '',
+    }
+    this.code = {}
+  }
+  backspace = (key, name) => {
+
+    let { code2, code3, code4 } = this.state;
+
+    if (key.nativeEvent.key === "Backspace") {
+      if (name == "code2") {
+        if (code2.length == 0) {
+          this.setState({ code1: "" })
+          this.code["code1"].focus();
+        }
+        else this.code["code2"].focus()
+      }
+
+      else if (name == "code3") {
+        if (code3.length == 0) {
+          this.setState({ code2: "" })
+          this.code["code2"].focus();
+        }
+        else this.code["code3"].focus()
+      }
+
+      else if (name == "code4") {
+        if (code4.length == 0) {
+          this.setState({ code3: "" })
+          this.code["code3"].focus();
+        }
+        else this.code["code4"].focus()
+      }
     }
   }
+  clearCode = () => {
+    this.code["code1"].focus()
+    this.setState({
+      code1: '',
+      code2: '',
+      code3: '',
+      code4: '',
+    })
+  }
+  onChangeCode = (name, text) => {
 
-  _handleBarCodeScanned = () => {
+    let { code1, code2, code3, code4 } = this.state;
+    const textLength = text.length
+    this.setState({
+      [name]: text,
+    })
+
+    if (name == "code1") {
+      code1 = text
+      if (textLength == 0) this.code["code1"].focus()
+      else this.code["code2"].focus()
+    }
+
+    else if (name == "code2") {
+      code2 = text
+      if (textLength == 0) this.code["code2"].focus()
+      else this.code["code3"].focus()
+    }
+
+    else if (name == "code3") {
+      code3 = text
+      if (textLength == 0) this.code["code3"].focus()
+      else this.code["code4"].focus()
+    }
+
+    else if (name == "code4") {
+      code4 = text
+      if (textLength == 0) this.code["code4"].focus()
+    }
+
+    const code = code1 + code2 + code3 + code4;
+
+    if (code.length == 4) {
+      Keyboard.dismiss()
+      this._handleBarCodeScanned(code);
+    }
+
+  }
+
+
+  _handleBarCodeScanned = (pin) => {
     this.setState({ error: "", loader: true })
 
-    const { pin } = this.state
+    // const { pin } = this.state
     const { addNewReceipt, navigation } = this.props;
     var regex = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/;
 
@@ -38,6 +122,7 @@ class BarCodeScannerComponent extends React.Component {
         console.log(res)
         if (!regex.test(res)) {
           this.setState({ error: "Please enter correct pin number", loader: false })
+          this.code["code4"].focus()
         }
         else {
           let url = res.slice(res.indexOf('url=') + 4, res.lastIndexOf('"'));
@@ -60,7 +145,7 @@ class BarCodeScannerComponent extends React.Component {
   };
 
   render() {
-    const { loader, pin, error } = this.state;
+    const { loader, pin, error, code1, code2, code3, code4 } = this.state;
     return (
       // <QRCodeScanner
       //   showMarker
@@ -89,7 +174,7 @@ class BarCodeScannerComponent extends React.Component {
       //   }
       // />
       <View style={{ flex: 1, width: "100%" }}>
-        <ScrollView contentContainerStyle={{ justifyContent: "center", alignItems: "center" }} style={{ width: "100%" }}>
+        <ScrollView keyboardDismissMode="interactive" keyboardShouldPersistTaps="handled" contentContainerStyle={{ justifyContent: "center", alignItems: "center" }} style={{ width: "100%" }}>
           <View style={{ height: height - 100, width: "100%", justifyContent: "center", alignItems: "center" }}>
             <View style={{ width: "100%", justifyContent: "center", alignItems: "center" }}>
 
@@ -97,8 +182,8 @@ class BarCodeScannerComponent extends React.Component {
                 <Text style={{ fontSize: 40 }}>PaprClip</Text>
               </View>
 
-              <View style={{ width: "80%", marginVertical: 20, borderBottomWidth: 1, borderBottomColor: loader ? "lightgray" : "#04A5CF" }}>
-                <TextInput
+              <View style={{ width: "80%", marginVertical: 20, borderBottomColor: loader ? "lightgray" : "#04A5CF", justifyContent: "space-between", flexDirection: "row" }}>
+                {/* <TextInput
                   editable={!loader}
                   value={pin}
                   onChangeText={(pin) => {
@@ -113,15 +198,19 @@ class BarCodeScannerComponent extends React.Component {
                   selectionColor="#04A5CF"
                   style={{ width: "100%", fontSize: 25, textAlign: "center", color: "#04A5CF" }}
                   autoFocus
-                />
+                /> */}
+                <TextInput onKeyPress={(key) => { this.backspace(key, 'code1') }} editable={!loader} ref={ref => { this.code['code1'] = ref }} value={code1} onChangeText={(text) => { this.onChangeCode("code1", text) }} style={{ fontSize: 25, textAlign: "center", color: "#04A5CF", width: "15%", height: "100%", borderBottomColor: loader ? "lightgray" : code1 ? "#04A5CF" : "lightgray", borderBottomWidth: 1 }} keyboardType="number-pad" maxLength={1} selectionColor={"#04A5CF"} autoFocus />
+                <TextInput onKeyPress={(key) => { this.backspace(key, 'code2') }} editable={!loader} ref={ref => { this.code['code2'] = ref }} value={code2} onChangeText={(text) => { this.onChangeCode("code2", text) }} style={{ fontSize: 25, textAlign: "center", color: "#04A5CF", width: "15%", height: "100%", borderBottomColor: loader ? "lightgray" : code2 ? "#04A5CF" : "lightgray", borderBottomWidth: 1 }} keyboardType="number-pad" maxLength={1} selectionColor={"#04A5CF"} />
+                <TextInput onKeyPress={(key) => { this.backspace(key, 'code3') }} editable={!loader} ref={ref => { this.code['code3'] = ref }} value={code3} onChangeText={(text) => { this.onChangeCode("code3", text) }} style={{ fontSize: 25, textAlign: "center", color: "#04A5CF", width: "15%", height: "100%", borderBottomColor: loader ? "lightgray" : code3 ? "#04A5CF" : "lightgray", borderBottomWidth: 1 }} keyboardType="number-pad" maxLength={1} selectionColor={"#04A5CF"} />
+                <TextInput onKeyPress={(key) => { this.backspace(key, 'code4') }} editable={!loader} ref={ref => { this.code['code4'] = ref }} value={code4} onChangeText={(text) => { this.onChangeCode("code4", text) }} style={{ fontSize: 25, textAlign: "center", color: "#04A5CF", width: "15%", height: "100%", borderBottomColor: loader ? "lightgray" : code4 ? "#04A5CF" : "lightgray", borderBottomWidth: 1 }} keyboardType="number-pad" maxLength={1} selectionColor={"#04A5CF"} />
               </View>
 
               {loader ?
-                <ActivityIndicator size="large" color="black" />
+                <ActivityIndicator size="large" color="lightgray" />
                 :
                 <View style={{ width: "100%", flexDirection: "row", justifyContent: "center", alignItems: "center", marginVertical: 10 }}>
-                  <TouchableOpacity onPress={this._handleBarCodeScanned} style={{ marginHorizontal: 10, padding: 10 }}>
-                    <Text style={{ fontSize: 20 }}>Submit</Text>
+                  <TouchableOpacity onPress={this.clearCode} style={{ marginHorizontal: 10, padding: 10 }}>
+                    <Text style={{ fontSize: 20 }}>Clear</Text>
                   </TouchableOpacity>
                   <TouchableOpacity onPress={this._moveBackToReceiptsPage} style={{ marginHorizontal: 10, padding: 10 }}>
                     <Text style={{ fontSize: 20 }}>Cancel</Text>
